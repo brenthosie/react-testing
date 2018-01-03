e962f7de916bc65de46e85f66f7add18547e3a05
Brent Hosie <brent.hosie@rainfocus.com>
2018-01-03 16:55:28 -0700

    Test stubs starting point

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import { shallow } from 'enzyme'
+// import App from '../App'
+
+// this is an example of how to skip a test:
+it('When the App is rendered, isToggled begins as false')
+
+// test('When the App is rendered, isToggled begins as false', () => {
+// })
+//
+// test('When the App is rendered, isButtonDisabled begins as false', () => {
+// })
+//
+// test('When the ToggleButton is clicked and isToggled is false, isToggled becomes true', () => {
+// })
+//
+// test('When the ToggleButton is clicked and isToggled is true, isToggled becomes false', () => {
+// })
+//
+// test('When the checkbox is clicked and isButtonDisabled is false, isButtonDisabled becomes true', () => {
+// })
+//
+// test('When the checkbox is clicked and isButtonDisabled is false, isButtonDisabled becomes true', () => {
+// })
+//
+// test('The checkbox is driven by state.isButtonDisabled', () => {
+// })
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,10 @@
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from '../App'
+
+// this is an example of how to skip a test:
+it('renders without crashing')
+
+// test('renders without crashing', () => {
+// })
